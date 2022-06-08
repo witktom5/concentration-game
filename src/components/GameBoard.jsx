23d3d6e1c5fa9bd44c121ab255abffe8cfd11b76
witktom5 <witktom5@gmail.com>
2022-06-08 12:39:25 +0200

    add img preload

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -3,7 +3,21 @@ import GameContext from '../context/GameContext';
 import gameCards from '../gameCards';
 import GameCard from './GameCard';
 
+//  images for preload
+import Image1 from '../assets/img/1.jpg';
+import Image2 from '../assets/img/2.jpg';
+import Image3 from '../assets/img/3.jpg';
+import Image4 from '../assets/img/4.jpg';
+import Image5 from '../assets/img/5.jpg';
+import Image6 from '../assets/img/6.jpg';
+import Image7 from '../assets/img/7.jpg';
+import Image8 from '../assets/img/8.jpg';
+import reverse from '../assets/img/reverse.jpg';
+import bg from '../assets/img/bg.jpg';
+//
+
 function GameBoard() {
+  const [imgsLoading, setImgsLoading] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const { cards, setCards, removedCards, setRemovedCards, moves, setMoves } =
     useContext(GameContext);
@@ -26,8 +40,22 @@ function GameBoard() {
     setIsLoading(true);
   };
 
-  return isLoading ? (
-    <div>Loading</div>
+  return isLoading || imgsLoading ? (
+    <>
+      <div className='opacity-0 absolute'>
+        <img src={Image1} alt='' />
+        <img src={Image2} alt='' />
+        <img src={Image3} alt='' />
+        <img src={Image4} alt='' />
+        <img src={Image5} alt='' />
+        <img src={Image6} alt='' />
+        <img src={Image7} alt='' />
+        <img src={Image8} alt='' />
+        <img src={bg} alt='' />
+        <img src={reverse} onLoad={setImgsLoading(false)} alt='' />
+      </div>
+      <div>Loading</div>
+    </>
   ) : (
     <>
       {removedCards < 16 ? (
